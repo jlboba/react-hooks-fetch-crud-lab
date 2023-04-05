5804a394a3bbd6ba542855bd787e09c31203d901
Jerrica Bobadilla <jbobadilla27@gmail.com>
2023-04-05 09:42:59 -0700

    test

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+/*
+App
+|-ShoppingList
+    |-ItemForm
+    |-Filter
+    |-Item
+
+|-Header
+
+
+
+*/
+
+
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import Header from "./Header";
+
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  return (
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
